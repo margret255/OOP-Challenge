fc08ccb8ef8c3774ba2b3262bc6718789fda21c9
Wangui Margaret <148498603+margret255@users.noreply.github.com>
2025-04-18 08:19:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from pet import Pet
+
+# My digital pet
+my_pet = Pet("Buddy")
+
+# Trying out some methods
+my_pet.get_status()
+my_pet.eat()
+my_pet.play()
+my_pet.sleep()
+my_pet.train("roll over")
+my_pet.train("shake paw")
+my_pet.show_tricks()
+my_pet.get_status()
 
